56311654715d36d0d1cb37bffc6651fb0b8b66ae
scottrepulles <scottrepulles@gmail.com>
2025-04-02 23:51:10 +0800

    Add project files.

---
+++ DHK.E2E.Tests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+// To run functional tests for ASP.NET Web Forms and ASP.NET Core Blazor XAF Applications,
+// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
+//
+// -For Google Chrome: download "chromedriver.exe" from https://chromedriver.chromium.org/downloads.
+// -For Microsoft Edge: download "msedgedriver.exe" from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/.
+//
+// Selenium requires a path to the downloaded driver. Add a folder with the driver to the system's PATH variable.
+//
+// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
+
+namespace DHK.Module.E2E.Tests;
+
+public class DHKTests : IDisposable {
+    const string BlazorAppName = "DHKBlazor";
+    const string AppDBName = "DHK";
+    EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+
+    public DHKTests() {
+        FixtureContext.RegisterApplications(
+            new BlazorApplicationOptions(BlazorAppName, string.Format(@"{0}\..\..\..\..\DHK.Blazor.Server", Environment.CurrentDirectory))
+        );
+        FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "DHKEasyTest", server: @"(localdb)\mssqllocaldb"));
+    }
+    public void Dispose() {
+        FixtureContext.CloseRunningApplications();
+    }
+    [Theory]
+    [InlineData(BlazorAppName)]
+    public void TestBlazorApp(string applicationName) {
+        FixtureContext.DropDB(AppDBName);
+        var appContext = FixtureContext.CreateApplicationContext(applicationName);
+        appContext.RunApplication();
+        appContext.GetForm().FillForm(("User Name", "Admin"));
+        appContext.GetAction("Log In").Execute();
+        Assert.True(appContext.Navigate("My Details"));
+        Assert.True(appContext.Navigate("Role"));
+        Assert.True(appContext.Navigate("Users"));
+        Assert.True(appContext.Navigate("Reports.Dashboards"));
+        Assert.True(appContext.Navigate("Reports.Reports"));
+        Assert.True(appContext.Navigate("State Machine.State Machine"));
+    }
+}
